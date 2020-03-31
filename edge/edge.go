7a7dbf8a8fab60219e7848b964667d750de18b58
Rob Fielding <rfielding@pop-os.localdomain>
2020-03-31 11:35:46 -0400

    basic expiration

--- edge/edge.go
+++ edge/edge.go
@@ -1,8 +1,10 @@
 package edge
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/rfielding/principia/common"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"time"
@@ -37,6 +39,11 @@ type Listener struct {
 	Lsn            net.Listener
 }
 
+type Required struct {
+	Name string
+	Port int
+}
+
 // Edge is pointed to by Peer, and contains the reverse proxy to
 // spawned Listener objects
 type Edge struct {
@@ -48,13 +55,120 @@ type Edge struct {
 	Listeners    []Listener
 	DefaultLease time.Duration
 	Peers        []Peer
+	Required     []Required
+}
+
+type Item struct {
+	Endpoint   string
+	Volunteers []string
+	Expose     bool
+}
+
+// TODO: upgrade all to https
+func (e *Edge) AvailableFromPeer(peer Peer) map[string]*Item {
+	url := fmt.Sprintf("http://%s:%d/available", peer.Host, peer.Port)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		e.Logger("error creating search %s for peer: %v", url, err)
+		return nil
+	}
+	cl := http.Client{}
+	res, err := cl.Do(req)
+	if err != nil {
+		e.Logger("error searching peer: %v", err)
+		return nil
+	}
+	if res.StatusCode != http.StatusOK {
+		e.Logger("error talking to %s peer: %d", url, res.StatusCode)
+		res.Body.Close()
+		return nil
+	}
+	j, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		e.Logger("unable to read body from %s: %v", url, err)
+		res.Body.Close()
+		return nil
+	}
+	res.Body.Close()
+	var items map[string]*Item
+	err = json.Unmarshal(j, &items)
+	if err != nil {
+		e.Logger("Unable to marshal response from %s: %v", url, err)
+		return nil
+	}
+	return items
+}
+
+// Available should be periodically polled for
+// ports available to service us
+func (e *Edge) Available() map[string]*Item {
+	available := make(map[string]*Item)
+	// These are locally implemented
+	for _, lsn := range e.Listeners {
+		available[lsn.Name] = &Item{
+			Endpoint: fmt.Sprintf("127.0.0.1:%d", lsn.Port),
+			Expose:   lsn.Expose,
+		}
+	}
+	// These exist remotely
+	for _, rq := range e.Required {
+		available[rq.Name] = &Item{
+			Endpoint:   fmt.Sprintf("127.0.0.1:%d", rq.Port),
+			Volunteers: make([]string, 0),
+		}
+	}
+	// We narrow it down to which peers implement this
+	for p, peer := range e.Peers {
+		peerAddr := fmt.Sprintf("%s:%d", peer.Host, peer.Port)
+		items := e.AvailableFromPeer(peer)
+		if items == nil && time.Now().Unix() > e.Peers[p].ExpiresAt.Unix() {
+			// it's expired and we could not contact it
+		} else {
+			if items != nil {
+				e.Peers[p].ExpiresAt = time.Now().Add(e.DefaultLease)
+				for kName, _ := range items {
+					for _, rq := range e.Required {
+						if kName == rq.Name {
+							available[kName].Volunteers =
+								append(
+									available[kName].Volunteers,
+									peerAddr,
+								)
+						}
+					}
+				}
+			}
+		}
+	}
+	// Sort peers by expiration time
+	peerCount := len(e.Peers)
+	now := time.Now()
+	for i := 0; i < peerCount; i++ {
+		for j := 0; j < peerCount; j++ {
+			if e.Peers[i].ExpiresAt.Unix() > e.Peers[j].ExpiresAt.Unix() {
+				tmp := e.Peers[i]
+				e.Peers[i] = e.Peers[j]
+				e.Peers[j] = tmp
+			}
+		}
+	}
+	// Cut off expired peers
+	i := 0
+	for i = 0; i < peerCount; i++ {
+		if now.Unix() > e.Peers[i].ExpiresAt.Unix() {
+			break
+		}
+	}
+	e.Peers = e.Peers[0:i]
+
+	return available
 }
 
 // ServeHTTP serves up http for this service
 func (e *Edge) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		if r.RequestURI == "/config" {
-			w.Write(common.AsJsonPretty(e))
+		if r.RequestURI == "/available" {
+			w.Write(common.AsJsonPretty(e.Available()))
 			return
 		}
 	}
@@ -62,6 +176,9 @@ func (e *Edge) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // Tells us to listen internally on a port
 func (e *Edge) Spawn(lsn Listener) error {
+	if lsn.Name == "" {
+		lsn.Name = e.Name
+	}
 	if lsn.Port == 0 {
 		lsn.Port = AllocPort()
 		if lsn.PortIntoCmdArg > 0 {
@@ -92,7 +209,10 @@ func (e *Edge) Peer(host string, port int) {
 
 func (e *Edge) Requires(listener string, port int) {
 	e.Logger("e.Requires: %s %d", listener, port)
-	//Use local Listeners or serve up peer Listeners
+	e.Required = append(e.Required, Required{
+		Name: listener,
+		Port: port,
+	})
 }
 
 func AllocPort() int {
@@ -102,9 +222,6 @@ func AllocPort() int {
 }
 
 func Start(e *Edge) *Edge {
-	if e.Name != "" && e.Logger == nil {
-		e.Logger = common.NewLogger(e.Name)
-	}
 	// Allocate a port if not specified
 	if e.Port == 0 {
 		e.Port = AllocPort()
@@ -115,9 +232,13 @@ func Start(e *Edge) *Edge {
 	if e.Bind == "" {
 		e.Bind = "0.0.0.0"
 	}
+	if e.Name != "" && e.Logger == nil {
+		e.Logger = common.NewLogger(e.Name)
+	}
 	e.Listeners = make([]Listener, 0)
 	e.DefaultLease = time.Duration(30 * time.Second)
 	e.Peers = make([]Peer, 0)
+	e.Required = make([]Required, 0)
 	e.Logger("edge.Start: https://%s:%d", e.Host, e.Port)
 	go http.ListenAndServe(fmt.Sprintf("%s:%d", e.Bind, e.Port), e)
 	return e
--- edge/edge_test.go
+++ edge/edge_test.go
@@ -2,6 +2,7 @@ package edge_test
 
 import (
 	"fmt"
+	"github.com/rfielding/principia/common"
 	"github.com/rfielding/principia/edge"
 	"testing"
 )
@@ -14,33 +15,48 @@ func TryTest(t *testing.T, err error) {
 }
 
 func TestEdge(t *testing.T) {
+	eAuth := edge.Start(&edge.Edge{
+		Name: "eAuth",
+	})
+	TryTest(t, eAuth.Spawn(edge.Listener{
+		PortIntoEnv: "EAUTH_PORT",
+		Cmd:         []string{"/usr/bin/authsvr"},
+	}))
 
 	// This is a sidecar for a database on random port
 	eDB := edge.Start(&edge.Edge{
-		Name: "proxy_eDB_eWeb",
+		Name: "eDB_eWeb",
 	})
 	TryTest(t, eDB.Spawn(edge.Listener{
-		Name:           "eDB_eWeb",
-		PortIntoCmdArg: 2,           // write into an arg
-		PortIntoEnv:    "EWEB_PORT", // or write into an env
+		PortIntoCmdArg: 2, // write into an arg
 		Cmd:            []string{"/usr/bin/edb", "-p", "????", "-s", "eWeb"},
 	}))
 
 	// This is a proxy on 8122 to a web server on 8123, talking to db on
 	eWeb := edge.Start(&edge.Edge{
-		Name: "proxy_eWeb",
+		Name: "eWeb",
 	})
 	// Allocate an arbitrary port for the db
 	eDB_eWeb_port := edge.AllocPort()
 	eDB_eWeb_ports := fmt.Sprintf("%d", eDB_eWeb_port)
+	eAuth_port := edge.AllocPort()
+	eAuth_ports := fmt.Sprintf("%d", eAuth_port)
 	// Spawn the web server talking to the db
 	TryTest(t, eWeb.Spawn(edge.Listener{
-		Name:           "eWeb",
-		Expose:         true,
-		PortIntoCmdArg: 2,
-		Cmd:            []string{"/usr/bin/eWeb", "-p", "????", "-dbp", eDB_eWeb_ports},
-		Env:            []string{"EWEB_PORT", eDB_eWeb_ports},
+		Expose:      true,
+		Cmd:         []string{"/usr/bin/eWeb"},
+		PortIntoEnv: "EWEB_PORT",
+		Env: []string{
+			"EDB_PORT", eDB_eWeb_ports,
+			"EAUTH_PORT", eAuth_ports,
+		},
 	}))
 	eWeb.Peer(eDB.Host, eDB.Port)
+	eWeb.Peer(eAuth.Host, eAuth.Port)
 	eWeb.Requires("eDB_eWeb", eDB_eWeb_port)
+	eWeb.Requires("eAuth", eAuth_port)
+
+	fmt.Printf("Available eAuth:%d %s", eAuth.Port, common.AsJsonPretty(eAuth.Available()))
+	fmt.Printf("Available eDB:%d %s", eDB.Port, common.AsJsonPretty(eDB.Available()))
+	fmt.Printf("Available eWeb:%d %s", eWeb.Port, common.AsJsonPretty(eWeb.Available()))
 }
